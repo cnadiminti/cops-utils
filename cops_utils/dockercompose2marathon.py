73bb1abb7f578718590cc0a0e4c63c29244d2cab
Chandra Mohan Babu Nadiminti <cnadimin@cisco.com>
2016-08-11 19:43:53 -0700

    Test pre-commit hook

---
+++ cops_utils/dockercompose2marathon.py
@@ -0,0 +1,11 @@
+import logging
+
+
+logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
+
+
+def main():
+    logging.warning('This is work-in-progress!!!')
+
+if __name__ == '__main__':
+    main()
